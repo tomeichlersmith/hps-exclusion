f5850b25564ded7e5e008ac1c8412a34b5ef6eb9
tomeichlersmith <eichl008@umn.edu>
2024-04-13 07:37:00 -0700

    load LUT on first call to function requiring it
    not storing the pickle file in git since I'll probably want to regenerate it at some point

--- __init__.py
+++ __init__.py
@@ -3,6 +3,23 @@
 Yellin (2003) https://arxiv.org/abs/physics/0203002
 """
 
+
+import numpy as np
+
+
+def _load_lookup_table():
+    """load the max intervals (separated by signal strength mu and number of events k)
+    lookup table from the pickle file
+
+    We look for the file in the same directory as this file and return
+    the lookup table for later usage.
+    """
+    import pathlib
+    import pickle
+    with open(pathlib.Path(__file__).parent.resolve() / 'interval_ntrials_10000.p', 'rb') as f:
+         return pickle.load(f)
+
+
 def k_largest_intervals(cumulants):
     """generate list of largest intervals according to the input cumulants
 
@@ -21,13 +38,13 @@ def k_largest_intervals(cumulants):
     ]
 
 
-def min_signal_strength_allowed(
+def max_signal_strength_allowed(
     data_cumulants, *,
     confidence_level = 0.9
 ):
-    """find the minimum signal strength that is allowed at the input confidence level
-    for the input data
-
+    """find the maximum signal strength that is allowed by the input data
+    at the input confidence level
+    
     This is an implementation of OIM, we
     - Determine the largest intervals for each possible k in the cumulants
     - Lookup the confidence of all the possible mu, k pairs in the lookup table
@@ -49,7 +66,13 @@ def min_signal_strength_allowed(
         >>> min_mu_with_best_k_over(data_cumulants).shape
         (200,)
     """
-                
+
+    if getattr(max_signal_strength_allowed, 'max_interval_trails_lut', None) is None:
+        max_signal_strength_allowed.max_interval_trails_lut = _load_lookup_table()
+        max_signal_strength_allowed.tested_signal_strengths = np.array(
+            list(max_signal_strength_allowed.max_interval_trails_lut.keys())
+        )
+    
     # apply the largest interval algorithm and store the largest intervals in
     # an array indexed by k
     max_interval_by_k = k_largest_intervals(data_cumulants)
@@ -58,13 +81,23 @@ def min_signal_strength_allowed(
     # 0th -> index of mu in lookupTable
     # 1st -> k
     # the rest -> other (non event) indices of data_cumulants array
-    confidence = np.full((len(lookupTable), len(max_interval_by_k), *data_cumulants.shape[:-1]), 0.)
-    for i_mu, mu in enumerate(lookupTable):
+    confidence = np.full(
+        (
+            len(max_signal_strength_allowed.max_interval_trails_lut),
+            len(max_interval_by_k),
+            *data_cumulants.shape[:-1]
+        ),
+        0.
+    )
+    for i_mu, mu in enumerate(max_signal_strength_allowed.max_interval_trails_lut):
         for k in range(len(max_interval_by_k)):
             confidence[i_mu,k,:] = np.sum(
-                np.less.outer(lookupTable[mu][k], max_interval_by_k[k]),
+                np.less.outer(
+                    max_signal_strength_allowed.max_interval_trails_lut[mu][k],
+                    max_interval_by_k[k]
+                ),
                 axis=0
-            )/10000 if k in lookupTable[mu] else 0.
+            )/10000 if k in max_signal_strength_allowed.max_interval_trails_lut[mu] else 0.
     # find k with best confidence for each mu
     best_conf_over_k = np.max(confidence, axis=1)
     # select mu indices where this confidence is above the input threshold
@@ -77,5 +110,5 @@ def min_signal_strength_allowed(
     # to how we constructed the array
     min_i_mu_above = np.argmax(i_mu_above_selection, axis=0)
     # now lookup the min mu using our min_i_mu
-    min_mu_above = np.array(list(lookupTable.keys()))[min_i_mu_above]
+    min_mu_above = max_signal_strength_allowed.tested_signal_strengths[min_i_mu_above]
     return min_mu_above
