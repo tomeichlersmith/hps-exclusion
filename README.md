a2330bf451957a3e5cfe30b62ff787a3837b1c54
tomeichlersmith <eichl008@umn.edu>
2024-04-22 13:23:44 -0700

    move docs on oim-specific into doc string, update readme

--- README.md
+++ README.md
@@ -1,34 +1,20 @@
-# `optimum_interval_method`
+# HPS Exclusion
+Helper modules for performing exclusions with HPS.
+
+### `optimum_interval_method`
 NumPy implementation of the Optimum Interval Method from Yellin (2003).
+
+### `production`
+Functional implementation for calculating the total signal production
+for a variety of dark photon masses.
+
+### `models`
+Implementation of different models functions for calculating decay rates
+and branching ratios.
 
 ## Install
 I don't plan to publish this package to PyPI but you can still install it from source.
 ```
-git clone https://github.com/tomeichlersmith/optimum_interval_method.git
-pip install --user optimum_interval_method/
+git clone https://github.com/tomeichlersmith/hps-exclusion.git
+pip install --user hps-exclusion/
 ```
-
-## Usage
-You must have a locally-available table of maximum interval samples in order to
-perform the Optimum Interval Method. This package allows for you to produce a table
-to your desired precision and size and then cache that table for later use.
-```python
-import optimum_interval_method as oim
-# define the table precision and size
-#  more signal strengths and more trials require more space in memory to hold the table
-#  below I show the default values which give /okay/ precision with a small table size
-oim.new(
-    max_signal_strenght = 20.0,
-    n_test_mu = 100,
-    n_trials = 1_000
-)
-```
-Using the table and performing the OIM then means loading the table and asking it to
-evaluate the maximum allowed signal strength given an input data set.
-```python
-o = oim.load()
-o.max_signal_strength_allowed(data)
-```
-The input `data` array is required to have the _last_ axis be the one indexing the events.
-These events should already be transformed into a uniformly distributed variable according
-to the signal model being tested.
--- src/optimum_interval_method/__init__.py
+++ src/optimum_interval_method/__init__.py
@@ -1,6 +1,31 @@
 """numpy implementation of the optimum interval method
 
 Yellin (2003) https://arxiv.org/abs/physics/0203002
+
+You must have a locally-available table of maximum interval samples in order to
+perform the Optimum Interval Method. This package allows for you to produce a table
+to your desired precision and size and then cache that table for later use.
+
+    import optimum_interval_method as oim
+    # define the table precision and size
+    #  more signal strengths and more trials require more space in memory to hold the table
+    #  below I show the default values which give /okay/ precision with a small table size
+    # load_or_new will check if the cache exists and only create a new table
+    # if the cache does not exist, use new instead if you want to overwrite the cache
+    oim.load_or_new(
+        max_signal_strenght = 20.0,
+        n_test_mu = 100,
+        n_trials = 1_000
+    )
+
+Using the table and performing the OIM then means loading the table and asking it to
+evaluate the maximum allowed signal strength given an input data set.
+
+    oim.max_signal_strength_allowed(data)
+
+The input `data` array is required to have the _last_ axis be the one indexing the events.
+These events should already be transformed into a uniformly distributed variable according
+to the signal model being tested.
 """
 
 
@@ -69,3 +94,31 @@ def load() -> _oim.OptimumIntervalMethod:
         with open(__cache_location, 'rb') as cache_file:
             return pickle.load(cache_file)
     raise ValueError('No cache file to load OIM table from. Make sure to call `new` to define the test signal strengths and the size of the table.')
+
+
+def load_or_new(**kwargs) -> _oim.OptimumIntervalMethod:
+    """Attempt to load the table from cache, choosing to create a new one with
+    the input keyword arguments if no cache is available."""
+
+    try:
+        return load()
+    except ValueError:
+        return new(**kwargs)
+
+
+def max_signal_strenght_allowed(*args, **kwargs):
+    """Usability function to hold the loaded table for the user
+
+    The user is required to already have a table in cache, so this
+    will error out if no table can be loaded from cache.
+
+    All arguments (both position and keyword) are passed to the
+    _oim.OptimumIntervalMethod.max_signal_strength_allowed function.
+    """
+    
+    if getattr(max_signal_strength_allowed, '_oim_calculator', None) is None:
+        max_signal_strength_allowed._oim_calculator = load()
+    return getattr(
+        max_signal_strength_allowed,
+        '_oim_calculator'
+    ).max_signal_strength_allowed(*args, **kwargs)
