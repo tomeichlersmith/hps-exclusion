e4d434af55b7595b47333fb9847430a2eff16b03
tomeichlersmith <eichl008@umn.edu>
2024-04-24 07:47:09 -0700

    add polynomial fits done on new simulation files

--- src/production/mass_resolution.py
+++ src/production/mass_resolution.py
@@ -3,3 +3,9 @@
 from ._polynomial import polynomial
 
 alic_2016_simps = polynomial(1.06314, 3.45955e-02, -6.62113e-5)
+"""Copied from Alic's 2016 SIMPs reach scripts"""
+
+tom_2016_vanilla_rereach = polynomial(9.10174583e-01, 2.94057744e-02, 8.63423108e-05)
+"""Tom produced with KF trackingon 2016 displaced vanilla A' signal files
+
+copied in 4/24/2024"""
--- src/production/radiative_fraction.py
+++ src/production/radiative_fraction.py
@@ -5,3 +5,12 @@ from ._polynomial import polynomial
 alic_2016_simps = polynomial(
     -1.04206e-1, 9.992547e-3, -1.99437e-4, 1.83534e-6, -7.93138e-9, 1.30456e-11
 )
+"""Alic produced with KF tracking on 2016 background files"""
+
+tom_2016_vanilla_rereach = polynomial(
+    -2.12037976e-01, 1.58881667e+01, -3.23546649e+02, 3.03753504e+03,
+    -1.33484525e+04, 2.20606661e+04
+)
+"""Tom produced with KF tracking on 2016 background files
+
+copied in 4/23/2024"""
