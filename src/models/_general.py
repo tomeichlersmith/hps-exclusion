1af1b77b2fc87d9d2a2f2e1c88b8c10f268ba86e
tomeichlersmith <eichl008@umn.edu>
2024-04-28 08:40:44 -0700

    add some doc on generate functions

--- src/models/_general.py
+++ src/models/_general.py
@@ -1,4 +1,16 @@
-"""base physics functions and constants useful for other models"""
+"""base physics functions and constants useful for other models
+
+Attributes
+----------
+c : float
+    speed of light in mm/s
+hbar : float
+    reduced Planck's constant in MeV s
+electron_mass: float
+    mass of electron in MeV
+alpha: float
+    fine structure constant in the low energy limit
+"""
 
 
 c = 3.00e11 #mm/s
@@ -8,6 +20,12 @@ alpha = 1.0 / 137.0
 
     
 def Beta(x, y):
+    """used in some decay rates, it is helpful to isolate this beta function here
+
+    $$
+        \beta(x,y) = (1+y^2-x^2-2y)(1+y^2-x^2+2y)
+    $$
+    """
     return (
         1
         + math.pow(y, 2)
@@ -22,4 +40,13 @@ def Beta(x, y):
 
 
 def ctau(rate):
+    """Calculate a decay length from a decay rate
+
+    We use c [mm/s] and hbar [MeV s] so the input rate should be
+    in units of MeV and the output decay length will be in mm.
+
+    $$
+        c\tau = \frac{c \hbar}{\Gamma}
+    $$
+    """
     return (c * hbar / rate)
